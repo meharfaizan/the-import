7022ce485ae22e74ae451f1fa0b0d8832570e250
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-02-22 16:23:11 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,17 @@
+#    This file is part of the Compressor distribution.
 #    Copyright (c) 2021 Danish_00
-#    Improved By @Zylern
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
 
 from decouple import config
 
@@ -10,8 +22,9 @@ try:
     DEV = 1664850827
     OWNER = config("OWNER","1430593323")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
+    THUMB = config("THUMBNAIL")
 except Exception as e:
-    print("Environment vars Missing! Exiting App.")
-    print(str(e))
+    LOGS.info("Environment vars Missing")
+    LOGS.info("something went wrong")
+    LOGS.info(str(e))
     exit(1)
